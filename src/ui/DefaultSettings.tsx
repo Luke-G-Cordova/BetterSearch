c709e9cb362f241ec8722e1d8e068c06f92507b1
Luke Cordova <lukethegreat000@gmail.com>
2023-06-16 16:36:33 -0700

    basic popup skeleton

---
+++ src/ui/DefaultSettings.tsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react';
+import * as React from 'react';
+
+/**
+ * exact match
+ * regular expression
+ * loose search
+ * loose search percent match
+ * case sensitive
+ * stop auto scroll
+ * maximum matches
+ * selection color
+ */
+export default function DefaultSettings() {
+  const [searchType, setSearchType] = useState(0);
+  const [looseSearchPercent, setLooseSearchPercent] = useState(0.75);
+  const [maximumMatches, setMaximumMatches] = useState(100);
+  const [selectionColor, setSelectionColor] = useState('#fbff00');
+  return (
+    <div className="defaultSettings">
+      <div className="searchType">
+        <div>Search Type</div>
+        <div className="inputWrapper">
+          <input
+            id="exactMatch"
+            type="checkbox"
+            onClick={() => setSearchType(0)}
+            checked={searchType === 0}
+          />
+          <label htmlFor="exactMatch">Exact Match</label>
+        </div>
+        <div className="inputWrapper">
+          <input
+            id="regularExpression"
+            type="checkbox"
+            onClick={() => setSearchType(1)}
+            checked={searchType === 1}
+          />
+          <label htmlFor="regularExpression">Regular Expression</label>
+        </div>
+        <div className="inputWrapper">
+          <input
+            id="looseSearch"
+            type="checkbox"
+            onClick={() => setSearchType(2)}
+            checked={searchType === 2}
+          />
+          <label htmlFor="looseSearch">Loose Search</label>
+        </div>
+      </div>
+      <div className="caseSensitive">
+        <div className="inputWrapper">
+          <input type="checkbox" id="case" disabled={searchType === 2} />
+          <label htmlFor="case">Case Sensitivity</label>
+        </div>
+      </div>
+      <div className="looseSearchPercent">
+        <div className="inputWrapper">
+          <label htmlFor="looseSearchSlider">Loose Search Sensitivity</label>
+          <input
+            id="looseSearchSlider"
+            type="range"
+            min="0"
+            max="1"
+            step=".01"
+            disabled={searchType !== 2}
+            value={looseSearchPercent}
+            onInput={(e) => {
+              setLooseSearchPercent(
+                Number((e.target as HTMLInputElement).value),
+              );
+            }}
+          />
+        </div>
+        <span>{looseSearchPercent}</span>
+      </div>
+      <div className="generalSettings">
+        <div className="inputWrapper">
+          <input id="autoScroll" type="checkbox" />
+          <label htmlFor="autoScroll">Stop Auto Scroll</label>
+        </div>
+        <div className="inputWrapper">
+          <input
+            id="maximumMatches"
+            type="number"
+            value={maximumMatches}
+            min="0"
+            onInput={(e) =>
+              setMaximumMatches(Number((e.target as HTMLInputElement).value))
+            }
+          />
+          <label htmlFor="maximumMatches">Maximum Matches</label>
+        </div>
+        <div className="inputWrapper">
+          <input
+            id="selectionColor"
+            type="color"
+            value={selectionColor}
+            onInput={(e) =>
+              setSelectionColor((e.target as HTMLInputElement).value)
+            }
+          />
+          <label htmlFor="selectionColor">Selection Color</label>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/ui/Popup.tsx
+++ src/ui/Popup.tsx
@@ -1,12 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as React from 'react';
 import './Popup.scss';
+import Logo from './Logo';
+import DefaultSettings from './DefaultSettings';
 
 export default function Popup() {
+  const [coms, setComs] = useState<chrome.commands.Command[]>([]);
   useEffect(() => {
     // Example of how to send a message to eventPage.ts.
     chrome.runtime.sendMessage({ popupMounted: true });
+    chrome.commands.getAll((commands) => {
+      console.log(commands);
+      setComs(commands);
+    });
   }, []);
 
-  return <div className="popupContainer">Hello, world!</div>;
+  return (
+    <div className="popupContainer">
+      <Logo />
+      <DefaultSettings />
+      {/* <ul>
+        {coms.map((com, i) => (
+          <li key={i}>{com.name}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
 }
