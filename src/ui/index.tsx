220066f6928c86988a8449b1a2113f4510f2c928
Luke Cordova <lukethegreat000@gmail.com>
2023-06-16 14:25:33 -0700

    use createRoot instead

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Popup from './Popup';
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-  ReactDOM.render(<Popup />, document.getElementById('popup'));
+  const container = document.getElementById('popup');
+  const root = createRoot(container);
+  root.render(<Popup />);
 });
