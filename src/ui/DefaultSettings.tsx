b4523755b3af6de2960e98c990230f50233c1130
Luke Cordova <lukethegreat000@gmail.com>
2023-07-05 22:09:39 -0700

    styling still needs work

--- src/ui/DefaultSettings.tsx
+++ src/ui/DefaultSettings.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import * as React from 'react';
+import { default as Toggle } from './Toggle';
 
 /**
  * exact match
@@ -19,91 +20,108 @@ export default function DefaultSettings() {
   return (
     <div className="defaultSettings">
       <div className="defaultSettingsHeader">Default Settings:</div>
-      <div className="topSection">
-        <div className="searchType">
-          <div className="searchTypeHeader">Search Type</div>
-          <div className="inputWrapper">
-            <input
-              id="exactMatch"
-              type="checkbox"
-              onChange={() => setSearchType(0)}
-              checked={searchType === 0}
-            />
-            <label htmlFor="exactMatch">Exact Match</label>
+      <div className="settingsArea">
+        <div className="searchTypes">
+          <form>
+            <div className={searchType === 0 ? 'current' : ''}>
+              <input
+                id="emType"
+                type="checkbox"
+                onChange={() => setSearchType(0)}
+                checked={searchType === 0}
+              />
+              <label htmlFor="emType">Exact Match</label>
+            </div>
+
+            <div className={searchType === 1 ? 'current' : ''}>
+              <input
+                id="rgType"
+                type="checkbox"
+                onChange={() => setSearchType(1)}
+                checked={searchType === 1}
+              />
+              <label htmlFor="rgType">Regex</label>
+            </div>
+
+            <div className={searchType === 2 ? 'current' : ''}>
+              <input
+                id="lType"
+                type="checkbox"
+                onChange={() => setSearchType(2)}
+                checked={searchType === 2}
+              />
+              <label htmlFor="lType">Loose</label>
+            </div>
+          </form>
+        </div>
+
+        <div className="searchTypeSettings">
+          <div
+            className="typeSetting emSettings"
+            style={{ display: searchType === 0 ? 'block' : 'none' }}
+          >
+            <Toggle htmlFor="Case Sensitivity" />
           </div>
-          <div className="inputWrapper">
-            <input
-              id="regularExpression"
-              type="checkbox"
-              onChange={() => setSearchType(1)}
-              checked={searchType === 1}
-            />
-            <label htmlFor="regularExpression">Regular Expression</label>
+          <div
+            className="typeSetting rgSettings"
+            style={{ display: searchType === 1 ? 'block' : 'none' }}
+          >
+            <Toggle htmlFor="Case Sensitivity" />
           </div>
-          <div className="inputWrapper">
-            <input
-              id="looseSearch"
-              type="checkbox"
-              onChange={() => setSearchType(2)}
-              checked={searchType === 2}
-            />
-            <label htmlFor="looseSearch">Loose Search</label>
+          <div
+            className="typeSetting lSettings"
+            style={{ display: searchType === 2 ? 'block' : 'none' }}
+          >
+            <div className="inputWrapper">
+              <label htmlFor="looseSearchSlider">
+                Loose Search Sensitivity
+              </label>
+              <input
+                id="looseSearchSlider"
+                type="range"
+                min="0"
+                max="1"
+                step=".01"
+                disabled={searchType !== 2}
+                value={looseSearchPercent}
+                onInput={(e) => {
+                  setLooseSearchPercent(
+                    Number((e.target as HTMLInputElement).value),
+                  );
+                }}
+              />
+            </div>
+            <span>{looseSearchPercent}</span>
           </div>
         </div>
-        <div className="sensitivities">
-          <div className="inputWrapper">
-            <input type="checkbox" id="case" disabled={searchType === 2} />
-            <label htmlFor="case">Case Sensitivity</label>
-          </div>
 
+        <div className="horizontalBreak"></div>
+
+        <div className="generalSettings">
+          <Toggle htmlFor="Auto Scroll" />
           <div className="inputWrapper">
-            <label htmlFor="looseSearchSlider">Loose Search Sensitivity</label>
+            <label htmlFor="maximumMatches">Maximum Matches</label>
             <input
-              id="looseSearchSlider"
-              type="range"
+              id="maximumMatches"
+              type="number"
+              value={maximumMatches}
               min="0"
-              max="1"
-              step=".01"
-              disabled={searchType !== 2}
-              value={looseSearchPercent}
-              onInput={(e) => {
-                setLooseSearchPercent(
-                  Number((e.target as HTMLInputElement).value),
-                );
-              }}
+              onInput={(e) =>
+                setMaximumMatches(Number((e.target as HTMLInputElement).value))
+              }
             />
           </div>
-          <span>{looseSearchPercent}</span>
-        </div>
-      </div>
-
-      <div className="generalSettings">
-        <div className="inputWrapper">
-          <input id="autoScroll" type="checkbox" />
-          <label htmlFor="autoScroll">Stop Auto Scroll</label>
-        </div>
-        <div className="inputWrapper">
-          <input
-            id="maximumMatches"
-            type="number"
-            value={maximumMatches}
-            min="0"
-            onInput={(e) =>
-              setMaximumMatches(Number((e.target as HTMLInputElement).value))
-            }
-          />
-          <label htmlFor="maximumMatches">Maximum Matches</label>
-        </div>
-        <div className="inputWrapper">
-          <input
-            id="selectionColor"
-            type="color"
-            value={selectionColor}
-            onInput={(e) =>
-              setSelectionColor((e.target as HTMLInputElement).value)
-            }
-          />
-          <label htmlFor="selectionColor">Selection Color</label>
+          <div className="inputWrapper">
+            <label htmlFor="selectionColor">Selection Color</label>
+            <input
+              id="selectionColor"
+              type="color"
+              value={selectionColor}
+              onInput={(e) =>
+                setSelectionColor((e.target as HTMLInputElement).value)
+              }
+            />
+          </div>
         </div>
       </div>
     </div>
