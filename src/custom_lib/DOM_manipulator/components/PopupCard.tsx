34a85949fd7a139e5692491cbf595c7cb624a25f
Luke Cordova <lukethegreat000@gmail.com>
2023-09-05 20:15:08 -0700

    add and delete inputs works

--- src/custom_lib/DOM_manipulator/components/PopupCard.tsx
+++ src/custom_lib/DOM_manipulator/components/PopupCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ReactShadowRoot from 'react-shadow-root';
 import Input from './Input';
 import { styles } from './Styles.css';
@@ -32,6 +32,14 @@ export default function PopupCard({
     y: startY + window.scrollY,
     detectBorder,
   });
+  const inputKeys = useRef(1);
+  const [inputs, setInputs] = useState([
+    <Input
+      ariaKey={0 + ''}
+      key={0}
+      nonDraggableRefElement={makeChildNonDraggable}
+    />,
+  ]);
   return (
     <better-search-popup-card
       style={{ left: pos.x + 'px', top: pos.y + 'px' }}
@@ -44,6 +52,22 @@ export default function PopupCard({
             id="BS-input-button"
             className="BSButton BSControlButton"
             ref={makeChildNonDraggable}
+            onClick={() => {
+              setInputs((arr) => {
+                if (arr.length < 6) {
+                  arr.push(
+                    <Input
+                      ariaKey={inputKeys.current + ''}
+                      key={inputKeys.current}
+                      nonDraggableRefElement={makeChildNonDraggable}
+                    />,
+                  );
+                  inputKeys.current += 1;
+                  return [...arr];
+                }
+                return arr;
+              });
+            }}
           >
             NEW
           </span>
@@ -57,8 +81,42 @@ export default function PopupCard({
           </span>
         </div>
         <div className="BS-break"></div>
-        <div id="BS-form-wrapper">
-          <Input nonDraggableRefElement={makeChildNonDraggable} />
+        <div
+          id="BS-form-wrapper"
+          onClick={(e) => {
+            if (
+              (e.target as HTMLElement).className.includes('BSDeleteButton')
+            ) {
+              setInputs((arr) => {
+                const idx = arr
+                  .reduce((prev, cur, i) => {
+                    prev.push(cur.key);
+                    return prev;
+                  }, [])
+                  .indexOf(
+                    (
+                      (e.target as HTMLElement).parentNode
+                        .parentNode as HTMLElement
+                    ).ariaLabel,
+                  );
+                arr.splice(idx, 1);
+                if (arr.length === 0) {
+                  arr.push(
+                    <Input
+                      ariaKey={inputKeys.current + ''}
+                      key={inputKeys.current}
+                      nonDraggableRefElement={makeChildNonDraggable}
+                    />,
+                  );
+                  inputKeys.current += 1;
+                }
+                return [...arr];
+              });
+            }
+          }}
+        >
+          {...inputs}
+          {/* <Input nonDraggableRefElement={makeChildNonDraggable} /> */}
         </div>
       </ReactShadowRoot>
     </better-search-popup-card>
